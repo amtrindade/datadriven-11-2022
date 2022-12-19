5198e8dd865b55e65df533bb9e3322cbf1312e03
Antonio Trindade <atrindade@umov.me>
2022-12-19 20:01:02 -0300

    Adiciona excel, screenshot

--- src/test/java/com/targettrust/core/BaseTest.java
+++ src/test/java/com/targettrust/core/BaseTest.java
@@ -1,14 +1,28 @@
 package com.targettrust.core;
 
+import static com.targettrust.core.DriverFactory.getDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
 public abstract class BaseTest {
 	
+	@AfterMethod
+	public void afterMethod(ITestResult testResult) throws IOException {
+		File scrShot = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrShot,	new File("target"+ File.separator + testResult.getName()+"_"+ new Date() + ".jpg"));		
+	}
+	
 	@AfterTest
-	public void tearDown() throws Exception {
-//		File scrShot = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
-//		FileUtils.copyFile(scrShot,	new File("target"+ File.separator + testName.getMethodName()+ ".jpg"));
-		
+	public void afterTest() throws Exception {		
 		DriverFactory.quitDriver();
 	}
 }
--- src/test/java/com/targettrust/test/CountriesDataDrivenTest.java
+++ src/test/java/com/targettrust/test/CountriesDataDrivenTest.java
@@ -2,10 +2,13 @@ package com.targettrust.test;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.targettrust.core.BaseTest;
+import com.targettrust.core.SpreadsheetData;
 import com.targettrust.page.CountryPage;
 import com.targettrust.page.IndexPage;
 
@@ -33,5 +36,23 @@ public class CountriesDataDrivenTest extends BaseTest{
 			{"Venezuela", "Venezuela"},
 			{"United States", "United States"}
 		};
+	}
+	
+	@Test(dataProvider = "countriesListExcell")
+	public void testSearchCountryExcell(String searchCountry, String expectedCountry) {		
+		indexPage = new IndexPage();
+		indexPage.openURL();
+		countryPage = indexPage.searchCountry(searchCountry);
+		
+		assertEquals(countryPage.getTitleH1(), expectedCountry);
+	}
+	
+	@DataProvider(name = "countriesListExcell")
+	public Object[][] dataProviderCountriesExcell() {
+		Object[][] testData = SpreadsheetData.readExcelData(
+				"Paises",
+				"src" + File.separator + "test" +File.separator + "resources" + File.separator + "paises.xls"
+				,"Dados");		
+		return testData;
 	}
 }
